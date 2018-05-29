55e2dcf5e96ea6418f54f9437416aba3c67454e0
Joel M. de Souza <contato@joelmdesouza.com>
2018-05-29 10:12:50 -0300

    refactor: Add props title

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class BookShelf extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired
+    }
+
     render() {
+        const { title } = this.props
+
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">Currently Reading</h2>
+                <h2 className="bookshelf-title">{title}</h2>
                 <div className="bookshelf-books">
                 <ol className="books-grid">
                     <li>
