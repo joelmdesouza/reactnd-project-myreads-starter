4c0c6c45bdba122eed809728e91d4d9a9167ab9e
Joel M. de Souza <contato@joelmdesouza.com>
2018-06-12 09:19:26 -0300

    refactror: Transform BookShelf to Stateless Functional Components

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import ListBooks from './ListBooks';
 
-class BookShelf extends Component {
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        books: PropTypes.array.isRequired,
-        onChangeShelf: PropTypes.func.isRequired
-    }
-
-    render() {
-        const { title, books, onChangeShelf } = this.props
-
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{title}</h2>
-                <div className="bookshelf-books">
-                    <ListBooks
-                        books={books}
-                        onChangeShelf={onChangeShelf}
-                    />
-                </div>
+const BookShelf = ({title, books, onChangeShelf}) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <ListBooks
+                    books={books}
+                    onChangeShelf={onChangeShelf}
+                />
             </div>
-        )
-    }
+        </div>
+    )
+}
+
+BookShelf.propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
 }
 
 export default BookShelf
